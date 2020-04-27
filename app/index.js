7659f10f45f5100377b1329fd26ccd4514b22b43
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-04-28 05:34:36 +0900

    変更

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Worlddddddd!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
