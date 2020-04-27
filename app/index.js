443bf925f5e398428548ccac638a77b76d0913d5
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-04-28 05:55:45 +0900

    コマンド周りを整理

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello Worldddd unko!'))
+app.get('/', (req, res) => res.send('Hello World !'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
