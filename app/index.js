947f6b19dbb6f2e4f222586e4a84fdeefc324b2d
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-04-28 14:56:44 +0900

    jsを更新

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello World !'))
+app.get('/', (req, res) => res.send('Hello World ec2 Test !'))
 
 app.listen(8000, () => console.log('Example app listening on port 8000!'))
