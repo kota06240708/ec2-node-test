bed2751d40ee03bb405554dc2fd14af4e370f2a2
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2020-04-29 02:44:01 +0900

    update: shellの場所を変更

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">app test test</h1>
+      <h1 class="title">app</h1>
       <h2 class="subtitle">My splendiferous Nuxt.js project</h2>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
